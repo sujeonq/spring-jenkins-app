9293a47037e0c13a5c97a448b1d959c60871200c
sujeonq <sujeonq@gmail.com>
2024-10-11 11:52:48 +0900

    first commit

---
+++ src/test/java/kr/boogi/SpringJenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.boogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
